26355196446e5e69f012a3103ad3fc9f76b19c83
ck <ck@gps-e.com>
2016-04-03 20:57:37 +0800

    improve code for color

--- color.c
+++ color.c
@@ -1,57 +1,64 @@
 /* 包含头文件 */
 #include "color.h"
 
+/*
+    y = ( 0.299f * r + 0.587f * g + 0.114f * b + 0  );
+    u = (-0.169f * r - 0.331f * g + 0.500f * b + 128);
+    v = ( 0.500f * r - 0.419f * g - 0.081f * b + 128);
+
+    iu = u - 128; iv = v - 128;
+    r = (y               + 1.402f * iv);
+    g = (y - 0.344f * iu - 0.714f * iv);
+    b = (y + 1.772f * iu              );
+*/
+
 // 内部全局变量定义
-static int u_0_344[256];
-static int u_1_772[256];
-static int v_1_402[256];
-static int v_0_714[256];
+static short u_0_344[256];
+static short u_1_772[256];
+static short v_1_402[256];
+static short v_0_714[256];
 
 // 函数实现
 void init_color_convert(void)
 {
     static int inited = 0;
-    int i;
-
-    if (inited) return;
-    for (i=0; i<256; i++)
-    {
-        u_0_344[i] = (int)(0.344 * (i - 128));
-        u_1_772[i] = (int)(1.772 * (i - 128));
-        v_1_402[i] = (int)(1.402 * (i - 128));
-        v_0_714[i] = (int)(0.714 * (i - 128));
+    if (!inited) {
+        int i;
+        for (i=0; i<256; i++)
+        {
+            u_0_344[i] = (short)(0.344 * (i - 128));
+            u_1_772[i] = (short)(1.772 * (i - 128));
+            v_1_402[i] = (short)(1.402 * (i - 128));
+            v_0_714[i] = (short)(0.714 * (i - 128));
+        }
+        inited = 1;
     }
-    inited = 1;
 }
 
-void yuv_to_rgb(int y, int u, int v, BYTE *r, BYTE *g, BYTE *b)
+void yuv_to_rgb(BYTE y, BYTE u, BYTE v, BYTE *r, BYTE *g, BYTE *b)
 {
-    int tr;
-    int tg;
-    int tb;
+    int tr = y              + v_1_402[v];
+    int tg = y - u_0_344[u] - v_0_714[v];
+    int tb = y + u_1_772[u];
 
-    /* YUV 值饱和处理 */
-    u = u > 0   ? u : 0;
-    v = v > 0   ? v : 0;
-    u = u < 255 ? u : 255;
-    v = v < 255 ? v : 255;
-
-    tr = y              + v_1_402[v];
-    tg = y - u_0_344[u] - v_0_714[v];
-    tb = y + u_1_772[u];
-
-    /* RGB 值饱和处理 */
-    tr = tr > 0   ? tr : 0;
-    tg = tg > 0   ? tg : 0;
-    tb = tb > 0   ? tb : 0;
-    tr = tr < 255 ? tr : 255;
-    tg = tg < 255 ? tg : 255;
-    tb = tb < 255 ? tb : 255;
-
-    *r = tr;
-    *g = tg;
-    *b = tb;
+    /* 饱和处理 */
+    *r = tr < 0 ? 0 : tr < 255 ? tr : 255;
+    *g = tg < 0 ? 0 : tg < 255 ? tg : 255;
+    *b = tb < 0 ? 0 : tb < 255 ? tb : 255;
 }
+
+void rgb_to_yuv(BYTE r, BYTE g, BYTE b, BYTE *y, BYTE *u, BYTE *v)
+{
+    int ty = (19595 * r + 38470 * g + 7471 * b) >> 16;
+    int tu = ((-11076 * r - 21692 * g) >> 16) + (b >> 1) + 128;
+    int tv = ((-27460 * g - 5308  * b) >> 16) + (r >> 1) + 128;
+
+    /* 饱和处理 */
+    *y = ty < 0 ? 0 : ty < 255 ? ty : 255;
+    *u = tu < 0 ? 0 : tu < 255 ? tu : 255;
+    *v = tv < 0 ? 0 : tv < 255 ? tv : 255;
+}
+
 
 
 
--- color.h
+++ color.h
@@ -9,7 +9,8 @@ extern "C" {
 #endif
 
 void init_color_convert(void);
-void yuv_to_rgb(int y, int u, int v, BYTE *r, BYTE *g, BYTE *b);
+void yuv_to_rgb(BYTE y, BYTE u, BYTE v, BYTE *r, BYTE *g, BYTE *b);
+void rgb_to_yuv(BYTE r, BYTE g, BYTE b, BYTE *y, BYTE *u, BYTE *v);
 
 #ifdef __cplusplus
 }
