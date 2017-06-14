16522a12a3df06c9d8ddda46a95303d1afd2704a
chenk <chenk@apical.com.cn>
2017-06-14 17:47:33 +0800

    improve code for color convert.

--- color.c
+++ color.c
@@ -35,7 +35,7 @@ void init_color_convert(void)
     }
 }
 
-void yuv_to_rgb(BYTE y, BYTE u, BYTE v, BYTE *r, BYTE *g, BYTE *b)
+void yuv_to_rgb(int y, int u, int v, BYTE *r, BYTE *g, BYTE *b)
 {
     int tr = y              + v_1_402[v];
     int tg = y - u_0_344[u] - v_0_714[v];
@@ -47,16 +47,16 @@ void yuv_to_rgb(BYTE y, BYTE u, BYTE v, BYTE *r, BYTE *g, BYTE *b)
     *b = tb < 0 ? 0 : tb < 255 ? tb : 255;
 }
 
-void rgb_to_yuv(BYTE r, BYTE g, BYTE b, BYTE *y, BYTE *u, BYTE *v)
+void rgb_to_yuv(BYTE r, BYTE g, BYTE b, int *y, int *u, int *v)
 {
     int ty = (19595 * r + 38470 * g + 7471 * b) >> 16;
     int tu = ((-11076 * r - 21692 * g) >> 16) + (b >> 1) + 128;
     int tv = ((-27460 * g - 5308  * b) >> 16) + (r >> 1) + 128;
 
     /* ±•∫Õ¥¶¿Ì */
-    *y = ty < 0 ? 0 : ty < 255 ? ty : 255;
-    *u = tu < 0 ? 0 : tu < 255 ? tu : 255;
-    *v = tv < 0 ? 0 : tv < 255 ? tv : 255;
+    *y = ty;
+    *u = tu;
+    *v = tv;
 }
 
 
--- color.h
+++ color.h
@@ -9,8 +9,8 @@ extern "C" {
 #endif
 
 void init_color_convert(void);
-void yuv_to_rgb(BYTE y, BYTE u, BYTE v, BYTE *r, BYTE *g, BYTE *b);
-void rgb_to_yuv(BYTE r, BYTE g, BYTE b, BYTE *y, BYTE *u, BYTE *v);
+void yuv_to_rgb(int y, int u, int v, BYTE *r, BYTE *g, BYTE *b);
+void rgb_to_yuv(BYTE r, BYTE g, BYTE b, int *y, int *u, int *v);
 
 #ifdef __cplusplus
 }
